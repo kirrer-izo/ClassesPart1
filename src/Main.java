14d87d3df3daf3526d9baa4cbc1036bc6318a792
Isaack Muchoki <isaackmuchoki55@gmail.com>
2026-01-08 09:30:15 +0300

    set data on objects defined on Car class in the Main class

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,14 @@ public class Main {
         System.out.println("make = " + car.getMake());
         System.out.println("model = " + car.getModel());
         car.describeCar();
+
+        Car targa = new Car();
+        targa.setMake("Porsche");
+        targa.setModel("Targa");
+        targa.setDoors(2);
+        targa.setConvertible(false);
+        targa.setColor("red");
+
+        targa.describeCar();
     }
 }
