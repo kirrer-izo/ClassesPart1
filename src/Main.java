543f2e9c1a1de63118c81e437bf2b0717ed1b0df
Isaack Muchoki <isaackmuchoki55@gmail.com>
2026-01-08 09:16:28 +0300

    added setter methods in the Car class to be able to update private objects outside the class

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         Car car = new Car();
+        car.setMake("Porsche");
 
         System.out.println("make = " + car.getMake());
         System.out.println("model = " + car.getModel());
