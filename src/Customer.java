52f7344eade0a54ad667de6ea4c93973178d61f3
Isaack Muchoki <isaackmuchoki55@gmail.com>
2026-01-09 10:27:02 +0300

    created class named Customer with three fields namely name, creditLimit, and emailAddress. Generated setter mathods of all three fields. Created three constructors. First constructor for all three fields which assign arguments directly to the instance variables. Second, no-args constructor, that calls another constructor, passing some literal values for each argument. Lastly, constructor with just name and email parameters, which also calls another constructor.

---
+++ src/Customer.java
@@ -0,0 +1,32 @@
+public class Customer {
+
+    private String name;
+    private double creditLimit;
+    private String emailAddress;
+
+    public Customer(String name, double creditLimit, String emailAddress) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.emailAddress = emailAddress;
+    }
+
+    public Customer() {
+        this("Kirrer", 1000, "kirrer@gmail.com");
+    }
+
+    public Customer (String name, String emailAddress) {
+        this (name, 500, emailAddress);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
 public class Main {
 
     public static void main(String[] args) {
-        Car car = new Car("GTR", "Black", 2, true);
+//        Car car = new Car("GTR", "Black", 2, true);
+//
+//        car.describeCar();
 
-        car.describeCar();
+        Customer customer = new Customer();
+        Customer customerLevi = new Customer("Levi",  "levi@gmail.com");
+        Customer customerChiloo = new Customer("Chiloo", 5000, "chiloo@gmail.com");
 
+        System.out.println("Hi " + customer.getName() +
+                ". Confirm this is your email address " + customer.getEmailAddress() +
+                ". Your credit limit is $" + customer.getCreditLimit());
+        System.out.println("Hi " + customerLevi.getName() +
+                ". Confirm this is your email address " + customerLevi.getEmailAddress() +
+                ". Your credit limit is $" + customerLevi.getCreditLimit());
+        System.out.println("Hi " + customerChiloo.getName() +
+                ". Confirm this is your email address " + customerChiloo.getEmailAddress() +
+                ". Your credit limit is $" + customerChiloo.getCreditLimit());
     }
 }
